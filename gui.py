31ad800be89318b2c41bb026f239c641ee7c8be4
Alec Li <li.ale@husky.neu.edu>
2019-04-02 15:20:58 -0400

    created basic website structure

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 #import mysql.connector
 
@@ -12,7 +14,7 @@ app = Flask(__name__)
 def getClimbs():
     #cursor.execute(insertqueryhere)
     #return render_template("climbing.html")
-    return render_template("climbing.html")
+    return render_template("mountainproject.html")
 
 if __name__ == "__main__":
     app.run()
