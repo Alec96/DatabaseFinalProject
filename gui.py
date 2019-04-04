c8cf510624c25e5566529b903952e1b732e0fb56
Alec Li <li.ale@husky.neu.edu>
2019-04-04 18:51:41 -0400

    added css to page, and managed to pull data from inputs fro mthe front end to the back end

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import logging
 
 app = Flask(__name__)
 #import mysql.connector
@@ -10,10 +11,26 @@ app = Flask(__name__)
 # cursor = cnx.cursor()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def getClimbs():
     #cursor.execute(insertqueryhere)
     #return render_template("climbing.html")
+    return render_template("mountainproject.html")
+
+@app.route('/', methods=['POST'])
+def queryClimbs():
+    #cursor.execute(insertqueryhere)
+    #return render_template("climbing.html")
+    logging.debug("sent a post request")
+    style = request.form.get("style_select")
+
+    min_rating = request.form.get("min_rating")
+    max_rating = request.form.get("min_rating")
+
+    min_grade = request.form.get("min_grade")
+    max_grade = request.form.get("max_grade")
+
+    height = request.form.get("height")
     return render_template("mountainproject.html")
 
 if __name__ == "__main__":
