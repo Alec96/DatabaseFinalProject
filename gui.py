da811b205b33d5f00a9c4f4b0ba0ddfc52cdc819
Alec Li <li.ale@husky.neu.edu>
2019-04-16 11:07:18 -0400

    added query for grade rating to tell if a climb is sandbagged or soft

--- gui.py
+++ gui.py
@@ -12,9 +12,15 @@ cnx = mysql.connector.connect(user=config.USER, password=config.PASSWORD,
 cursor = cnx.cursor()
 
 def getClimbsQuery(name="", style="not applicable", min_grade=1, max_grade=13, min_rating=1, max_rating=4, height=""):
-    query = "Select * from climb as init_climb " \
+    query = "Select climb_name, climb_description, beta, grade, avg_quality_rating, (grade - avg_sugg_grade), group_concat(type_name)" \
+            " from climb as init_climb " \
             "left join climb_type using(climb_id)" \
-            "left join type using(type_id)"
+            "left join type using(type_id) " \
+            "left join (select climb_id, AVG(suggested_grade) as avg_sugg_grade " \
+            "from suggested_grade " \
+            "group by climb_id) as sugg_grade_by_climb using (climb_id)"
+
+
     try:
         height =  int(height)
     except:
@@ -45,7 +51,7 @@ def getClimbsQuery(name="", style="not applicable", min_grade=1, max_grade=13, m
                     "left join user using(user_id) " \
                     "where climb_id = init_climb.climb_id and " \
                     "user_height >= " + str(height-5) + " and user_height <= " + str(height+5) + ") > 1"
-    sql = sql + " order by avg_quality_rating desc"
+    sql = sql + " group by climb_id order by avg_quality_rating desc"
     return sql, params
 
 @app.route('/', methods=['GET'])
@@ -56,16 +62,13 @@ def getClimbs():
 def queryClimbs():
 
     name = request.form.get("climb_name_input")
-
     style = request.form.get("style_select")
-
     min_rating = request.form.get("min_rating")
     max_rating = request.form.get("max_rating")
-
     min_grade = request.form.get("min_grade")
     max_grade = request.form.get("max_grade")
-
     height = request.form.get("height")
+
     query, params = getClimbsQuery(name, style, min_grade, max_grade, min_rating, max_rating, height)
     cursor.execute(query, params)
     rows = cursor.fetchall()
@@ -73,12 +76,24 @@ def queryClimbs():
     climb_arr = []
     for row in rows:
         climb = {}
-        climb['name'] = row[2]
-        climb['description'] = row[3]
-        climb['grade'] = "V" + str(row[6])
-        climb['rating'] = row[7]
-        climb['Grade Accuracy'] = row[10]
-        climb['style'] = row[11]
+        climb['name'] = row[0]
+        climb['description'] = row[1]
+        climb['beta'] = row[2]
+        climb['grade'] = "V" + str(row[3])
+        climb['rating'] = row[4]
+
+        gradeDif = row[5]
+        if gradeDif is None:
+            gradeDif = "No Suggested Ratings"
+        elif gradeDif >= .5:
+            gradeDif = "Easier than stated"
+        elif gradeDif <= -.5:
+            gradeDif = "Harder than stated"
+        else:
+            gradeDif = "Accurate"
+
+        climb['grade_accuracy'] = gradeDif
+        climb['style'] = row[6]
         climb_arr.append(climb)
 
     return render_template("mountainproject.html", climbs = climb_arr)
